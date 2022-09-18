1fdfecc12fd31a5f9a8a84a1c6d82d27e4c6bbd6
Spiderman103 <101935459+Spiderman103@users.noreply.github.com>
2022-09-18 15:33:02 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAUwBE4rSvu80Znf73mwIkgPZavQkZhUxw",
+  authDomain: "storytelling-app-f3c9d.firebaseapp.com",
+  databaseURL: "https://storytelling-app-f3c9d-default-rtdb.firebaseio.com",
+  projectId: "storytelling-app-f3c9d",
+  storageBucket: "storytelling-app-f3c9d.appspot.com",
+  messagingSenderId: "912630022544",
+  appId: "1:912630022544:web:274e03f0e8c5dfbb977315"
+  };
